b5c88c9aa2b0d9295ee053b781a7a212e02e1a70
Deepankar Agrawal <deepankar01agrawal@gmail.com>
2015-03-29 20:34:31 +0530

    Completed the Installing of Required modules

--- clean.sh
+++ clean.sh
@@ -1,9 +1,3 @@
-#! /bin/sh
+#! /bin/bash
+# removes the logs and cleans the folder
 
-#--script to clean the logs ans status files
-
-if (rm -f *.txt) then
-	echo "Success"
-else
-	echo "Failure"
-fi
--- required/required.sh
+++ required/required.sh
@@ -2,24 +2,67 @@
 #--------Script for Installing the Prequisits for Installing Openstack-------
 #--------The Script has to run on every node------------
 
-echo "---------------Updating Packages----------------" > "required_log.txt"
-date >> "required_log.txt"
-echo ""
-echo ""
+#to get the script location 
+SCRIPT=$(readlink -f "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
 
-if (sudo apt-get update >> "required_log.txt") then
-	 echo "Update.Success" > "required.txt"
- 	 echo "---------------Installing NTP----------------" >> "required_log.txt"
-	 date >> "required_log.txt" 
-	 echo ""
-	 echo ""
-	 if (sudo apt-get install -y ntp >> "required_log.txt") then
-                echo "ntp.Success" >> "required.txt"
+#clean up the logs folder
+rm -r $SCRIPTPATH/logs/*
+
+echo "---------------Updating Packages----------------" > "$SCRIPTPATH/logs/required_log.txt"
+date >> "$SCRIPTPATH/logs/required_log.txt"
+echo -e "\n" >> "$SCRIPTPATH/logs/required_log.txt"
+echo -e "\n" >> "$SCRIPTPATH/logs/required_log.txt"
+
+if (apt-get update >> "$SCRIPTPATH/logs/required_log.txt") then
+	 echo "Update.Success" > "$SCRIPTPATH/logs/required.txt"
+ 	 echo "---------------Installing NTP----------------" >> "$SCRIPTPATH/logs/required_log.txt"
+	 date >> "$SCRIPTPATH/logs/required_log.txt" 
+	 echo -e "\n" >> "$SCRIPTPATH/logs/required_log.txt"
+	 echo -e "\n" >> "$SCRIPTPATH/logs/required_log.txt"
+	 if (apt-get install -y ntp >> "$SCRIPTPATH/logs/required_log.txt") then
+                echo "ntp.Success" >> "$SCRIPTPATH/logs/required.txt"
          else
-                echo "ntp.Fail" >> "required.txt"
+                echo "ntp.Fail" >> "$SCRIPTPATH/logs/required.txt"
 		exit 1
          fi
+
+	#adding keyring
+	 echo "---------------Adding Keyring----------------" >> "$SCRIPTPATH/logs/required_log.txt"
+         date >> "$SCRIPTPATH/logs/required_log.txt" 
+         echo -e "\n" >> "$SCRIPTPATH/logs/required_log.txt"
+         echo -e "\n" >> "$SCRIPTPATH/logs/required_log.txt"
+	 if (apt-get install ubuntu-cloud-keyring >> "$SCRIPTPATH/logs/required_log.txt") then
+		echo  "keyring.Success" >> "$SCRIPTPATH/logs/required.txt"
+	 else
+		echo "keyring.Fail" >> "$SCRIPTPATH/logs/required.txt"
+		exit 1
+	 fi
+	 
+	#adding Juno Location
+	 echo "---------------Adding Juno Location----------------" >> "$SCRIPTPATH/logs/required_log.txt"
+         date >> "$SCRIPTPATH/logs/required_log.txt" 
+         echo -e "\n" >> "$SCRIPTPATH/logs/required_log.txt"
+         echo -e "\n" >> "$SCRIPTPATH/logs/required_log.txt"
+	 if (echo "deb http://ubuntu-cloud.archive.canonical.com/ubuntu" \ "trusty-updates/juno main" > /etc/apt/sources.list.d/cloudarchive-juno.list) then
+		echo "repositoryUpdate.Success" >> "$SCRIPTPATH/logs/required.txt"
+	 else
+		echo "repositoryUpdate.Fail" >> "$SCRIPTPATH/logs/required.txt"
+	 	exit 1
+	 fi	
+
+	#reupdating the  apt packages
+	 echo "---------------Reupdating the apt cache----------------" >> "$SCRIPTPATH/logs/required_log.txt"
+         date >> "$SCRIPTPATH/logs/required_log.txt" 
+         echo -e "\n" >> "$SCRIPTPATH/logs/required_log.txt"
+         echo -e "\n" >> "$SCRIPTPATH/logs/required_log.txt"
+	 if (apt-get update >> "$SCRIPTPATH/logs/required_log.txt") then
+		echo "Reupdate.Success" >>"$SCRIPTPATH/logs/required.txt"
+	 else
+		echo "Reupdate.Fail" >> "$SCRIPTPATH/logs/required.txt"
+		exit 1
+	 fi
 else
-	echo "Update.Fail" > "required.txt"
-	exit 1
+	 echo "Update.Fail" > "$SCRIPTPATH/logs/required.txt"
+	 exit 1
 fi
